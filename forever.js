484942caf24b92f423da3b8ef100063944862add
Daniel Saewitz <switz213@gmail.com>
2013-01-28 20:16:50 -0500

    Remove forever-monitor

--- forever.js
+++
@@ -1,5 +0,0 @@
-if (process.env.NODE_ENV === 'production') {
-  require('./src/bootstrap').listen(process.env.PORT || 3000);
-} else {
-  require('derby').run(__dirname + '/src/bootstrap', 3000);
-}
--- phishvids.js
+++ phishvids.js
@@ -1,8 +1,5 @@
-var forever = require('forever-monitor');
-var child = new (forever.Monitor)('forever.js');
-
-child.on('exit', function () {
-    console.log('server.js has exited after 10 restarts');
-});
-
-child.start();
+if (process.env.NODE_ENV === 'production') {
+  require('./src/bootstrap').listen(process.env.PORT || 3000);
+} else {
+  require('derby').run(__dirname + '/src/bootstrap', 3000);
+}
